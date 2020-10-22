b1ea3cfd9c705c1e7be3294e9fc221caa9f620bb
rolczynski <rafal.rolczynski@gmail.com>
2020-10-22 15:14:06 +0200

    Add aspect without analysis

---
+++ article/analysis/aspect_without.py
@@ -0,0 +1,58 @@
+import os
+import logging
+import pathlib
+from typing import Iterable
+
+import numpy as np
+import transformers
+import aspect_based_sentiment_analysis as absa
+from aspect_based_sentiment_analysis import LabeledExample
+from aspect_based_sentiment_analysis import Sentiment
+from joblib import Memory
+
+import utils
+
+logger = logging.getLogger('analysis.aspect_without')
+HERE = pathlib.Path(__file__).parent
+memory = Memory(HERE / 'outputs')
+PRETRAINED_MODEL_NAMES = {
+    'restaurant': 'absa/bert-rest-0.2',
+    'laptop': 'absa/bert-lapt-0.2'
+}
+
+
+def build_dataset(domain: str, unknown_token: str) -> Iterable[LabeledExample]:
+    examples = absa.load_examples('semeval', domain, test=True)
+    # Remove the information about an aspect
+    # (change aspect to the special [UNK] token)
+    convert = lambda e: LabeledExample(e.text, unknown_token, e.sentiment)
+    dataset = map(convert, examples)
+
+    # Process only positive/negative examples
+    condition = lambda e: e.sentiment in [Sentiment.positive, Sentiment.negative]
+    dataset = filter(condition, dataset)
+    return dataset
+
+
+@memory.cache
+def predict_without_aspect(domain: str) -> np.ndarray:
+    name = PRETRAINED_MODEL_NAMES[domain]
+    tokenizer = transformers.BertTokenizer.from_pretrained(name)
+    dataset = build_dataset(domain, unknown_token=tokenizer.unk_token)
+    nlp = absa.load(name)
+
+    metric = absa.training.ConfusionMatrix(num_classes=3)
+    confusion_matrix = nlp.evaluate(dataset, metric, batch_size=32)
+    confusion_matrix = confusion_matrix.numpy()
+    return confusion_matrix
+
+
+if __name__ == '__main__':
+    os.chdir(HERE)
+    utils.setup_logger(HERE / 'logs' / 'aspect-without.log')
+
+    logger.info('Begin Evaluation')
+    for dataset_domain in ['restaurant', 'laptop']:
+        matrix = predict_without_aspect(dataset_domain)
+        accuracy = np.diagonal(matrix).sum() / matrix.sum()
+        logger.info(f'Accuracy {dataset_domain:10}: {accuracy:.4f}')
---
+++ article/analysis/utils.py
@@ -0,0 +1,22 @@
+import logging
+
+
+def setup_logger(
+        file_path: str = None,
+        level: int = 20,
+        msg_format: str = None
+):
+    msg_format = msg_format or '%(asctime)s [%(levelname)s] [%(name)s] %(message)s'
+    root_logger = logging.getLogger('analysis')
+    root_logger.setLevel(level)
+    root_logger.propagate = False
+    formatter = logging.Formatter(msg_format, datefmt='%Y-%m-%d %H:%M:%S')
+    console = logging.StreamHandler()
+    console.setFormatter(formatter)
+
+    # Handle all messages from the logger (not set the handler level)
+    root_logger.addHandler(console)
+    if file_path:
+        file_handler = logging.FileHandler(file_path, mode='w')
+        file_handler.setFormatter(formatter)
+        root_logger.addHandler(file_handler)
