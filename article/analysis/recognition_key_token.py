00c0825679cc3ed83ee669a85ee0fd3f5b15f995
rolczynski <rafal.rolczynski@gmail.com>
2020-10-30 11:56:34 +0100

    Add fix to key token recognition analysis

--- article/analysis/recognition_key_token.py
+++ article/analysis/recognition_key_token.py
@@ -1,12 +1,16 @@
 import os
 import logging
 import pathlib
-from functools import partial
+import itertools
+from typing import List
+from typing import Tuple
+from collections import defaultdict
 
-from sklearn.metrics import confusion_matrix
 import numpy as np
 import aspect_based_sentiment_analysis as absa
+from sklearn.metrics import confusion_matrix
 from aspect_based_sentiment_analysis import Example
+from aspect_based_sentiment_analysis import PatternRecognizer
 from aspect_based_sentiment_analysis import PredictedExample
 from joblib import Memory
 
@@ -23,22 +27,23 @@ PRETRAINED_LM_NAMES = {
 
 
 def mask(example: PredictedExample, index: int = None) -> Example:
-    nlp.tokenizer.basic_tokenizer.never_split = {nlp.tokenizer.mask_token}
+    # Make sure that the pipeline `NLP` has prepared the basic tokenizer.
+    NLP.tokenizer.basic_tokenizer.never_split = {NLP.tokenizer.mask_token}
     tokens = list(example.text_tokens)  # Break the reference to the object.
     if index is None:
         best = 0    # The patterns have already been sorted.
         chosen_pattern = example.review.patterns[best]
         index = np.argmax(chosen_pattern.weights)
-    tokens[index] = nlp.tokenizer.mask_token
+    tokens[index] = NLP.tokenizer.mask_token
     new_example = Example(text=' '.join(tokens), aspect=example.aspect)
     return new_example
 
 
-def ground_truth_examples(domain: str):
-    dataset = absa.load_examples('semeval', domain, test=False)
+def ground_truth_examples(domain: str, test: bool):
+    dataset = absa.load_examples('semeval', domain, test)
     for i, example in enumerate(dataset):
         yield i, -1, example    # Predict without a mask.
-        [tokenized_example] = nlp.tokenize([example])
+        [tokenized_example] = NLP.tokenize([example])
         n = len(tokenized_example.text_tokens)
         for index in range(n):
             new_example = mask(tokenized_example, index)
@@ -46,36 +51,60 @@ def ground_truth_examples(domain: str):
 
 
 @memory.cache
-def ground_truth(domain: str) -> np.ndarray:
+def _ground_truth(domain: str, test: bool) -> np.ndarray:
     results = []
-    examples = ground_truth_examples(domain)
+    examples = ground_truth_examples(domain, test)
     batches = absa.utils.batches(examples, batch_size=32)
     for batch in batches:
         indices, mask_index, batch_examples = zip(*batch)
-        predictions = nlp.transform(batch_examples)
+        predictions = NLP.transform(batch_examples)
         y_hat = [e.sentiment.value for e in predictions]
         results.extend(zip(indices, mask_index, y_hat))
     return np.array(results)
 
 
+def ground_truth(domain: str, test: bool) -> float:
+    results = _ground_truth(domain, test)
+    d = defaultdict(set)
+    for i, mask_i, y_hat in results:
+        d[i].add(y_hat)
+    is_key_token = [len(classes) > 1 for classes in d.values()]
+    return sum(is_key_token) / len(is_key_token)
+
+
 @memory.cache
-def evaluate(recognizer: absa.PatternRecognizer, domain: str) -> np.ndarray:
-    nlp.professor.pattern_recognizer = recognizer
+def _evaluate(
+        recognizer: PatternRecognizer,
+        domain: str,
+        test: bool) -> np.ndarray:
+    # It's not a generator because we cache function results.
+    # The global variable `NLP` helps to avoid serializing the complex input.
+    NLP.professor.pattern_recognizer = recognizer
     results = []
-    dataset = absa.load_examples('semeval', domain, test=False)
+    dataset = absa.load_examples('semeval', domain, test)
     batches = absa.utils.batches(dataset, batch_size=32)
     for batch in batches:
-        predictions = nlp.transform(batch)
+        predictions = NLP.transform(batch)
         predictions = list(predictions)  # Needed to the comparison.
 
         new_batch = [mask(prediction) for prediction in predictions]
-        new_predictions = nlp.transform(new_batch)
+        new_predictions = NLP.transform(new_batch)
 
         y_ref = [e.sentiment.value for e in predictions]
         y_new = [e.sentiment.value for e in new_predictions]
         results.extend(zip(y_ref, y_new))
-    # It is not a generator because we cache function results.
     return np.array(results)
+
+
+def evaluate(
+        recognizer: PatternRecognizer,
+        domain: str,
+        test: bool) -> List[Tuple[float, np.ndarray]]:
+    results = _evaluate(recognizer, domain, test)
+    y_ref, y_new = results.T
+    matrix = confusion_matrix(y_ref, y_new)
+    acc = 1 - accuracy(matrix)  # The aim is to change a prediction.
+    return acc, matrix
 
 
 if __name__ == '__main__':
@@ -85,22 +114,29 @@ if __name__ == '__main__':
     accuracy = lambda m: np.diagonal(m).sum() / m.sum()
 
     logger.info('Begin Evaluation: the Key Token Recognition')
-    for dataset_domain in ['restaurant', 'laptop']:
-        lm_name = PRETRAINED_LM_NAMES[dataset_domain]
-        nlp = absa.load(lm_name)
-
-        reference = ground_truth(dataset_domain)
+    for TEST, DOMAIN in itertools.product([False, True], ['restaurant', 'laptop']):
+        NAME = PRETRAINED_LM_NAMES[DOMAIN]
+        NLP = absa.load(NAME)
+        max_score = ground_truth(DOMAIN, TEST)
 
         random = extension.RandomPatternRecognizer()
         attention = extension.AttentionPatternRecognizer(max_patterns=5)
         gradient = extension.GradientPatternRecognizer(max_patterns=5)
         basic = absa.BasicPatternRecognizer(max_patterns=5)
         recognizers = [random, attention, gradient, basic]
+        RESULTS = [evaluate(recognizer, DOMAIN, TEST) for recognizer in recognizers]
 
-        evaluate_domain = partial(evaluate, domain=dataset_domain)
-        results = map(evaluate_domain, recognizers)
-        matrices = map(confusion_matrix, results)
-        accuracies = map(accuracy, matrices)
-
-        list(accuracies)
-        logger.info(f'{dataset_domain.upper()} DOMAIN\n')
+        logger.info(f'{DOMAIN.upper()} {"TEST" if TEST else "TRAIN"} DOMAIN\n'
+                    f'Examples that have at least one key token: {max_score:.4f}\n\n'
+                    f'Random Pattern Recognizer\n'
+                    f'Acc.: {RESULTS[0][0]/max_score:.4f} ({RESULTS[0][0]:.4f})\n'
+                    f'Confusion Matrix (y_ref, y_new):\n{RESULTS[0][1]}\n\n'
+                    f'Attention Pattern Recognizer\n'
+                    f'Acc.: {RESULTS[1][0]/max_score:.4f} ({RESULTS[1][0]:.4f})\n'
+                    f'Confusion Matrix (y_ref, y_new):\n{RESULTS[1][1]}\n\n'
+                    f'Gradient Pattern Recognizer\n'
+                    f'Acc.: {RESULTS[2][0]/max_score:.4f} ({RESULTS[2][0]:.4f})\n'
+                    f'Confusion Matrix (y_ref, y_new):\n{RESULTS[2][1]}\n\n'
+                    f'Basic Pattern Recognizer\n'
+                    f'Acc.: {RESULTS[3][0]/max_score:.4f} ({RESULTS[3][0]:.4f})\n'
+                    f'Confusion Matrix (y_ref, y_new):\n{RESULTS[3][1]}\n\n')
